ff6eed613d73d899ef9c32cbaf3544e4da240c86
sarzixon <msarzala.work@gmail.com>
2021-11-30 01:40:50 +0100

    working on basic ui

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             title: 'Development',
+            template: "./src/index.html"
         }),
     ],
     devtool: 'inline-source-map',
